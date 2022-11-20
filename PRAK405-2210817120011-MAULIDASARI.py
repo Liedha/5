d03d460ca0e5299e63da860a035d7617dfd09f2f
Liedha <115003614+Liedha@users.noreply.github.com>
2022-11-20 23:29:00 +0700

    Update PRAK405-2210817120011-MAULIDASARI.py

--- PRAK405-2210817120011-MAULIDASARI.py
+++ PRAK405-2210817120011-MAULIDASARI.py
@@ -1,14 +1,23 @@
-a,b=input('Masukkan Angka : ').split()
-a=int(a); b=int(b)
-for i in range (1, a+1):
-    j=i
-    while j>1:
-        print('(%d * %d) + '%(j,b),end=' '); j-=1
-        x=1; hasil=(i*b)
-        while x<i:
-            hasil+=(x*b); x+=1
-            print("(%d * %d) = %d"%(j,b,hasil))
-y=1; z=1; total=0
-while y <=a:
-                total += z*b; z+= y+1; y +=1
-print('%d'%(total))
+for a in range (3) :
+    b, c = input().split()
+    b = int (b)
+    c = int (c)
+    i = 0
+    j = 0
+    k = 0
+    for d in range (1, b + 1) :
+        e = d
+        f = 1
+        h = (d * c)
+        while e > 1 :
+            e = e - 1
+            print("(%d" %e, "* %d) + " %c, end='')
+        while f < d :
+            h = h + (f * c)
+            f = f + 1
+        print("(%d" %e, "* %d)" %c, "= %d" %h)
+    while k <= b :
+        i = i + (j * c)
+        j = j + (k + 1)
+        k = k + 1
+    print("%d\n" %i)
